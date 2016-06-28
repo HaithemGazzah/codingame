073ff29bed430a14e905bd256e5f88e86fa3e243
Ospici Matthieu <matthieu.ospici@atos.net>
2016-06-28 15:08:21 +0200

    - add actions, some bugs remain

--- codebusters/cb.py
+++ codebusters/cb.py
@@ -5,10 +5,69 @@ from collections import deque
 import random
 
 
+def move_v(c):
+    return "MOVE " + str(c[0]) + " " + str(c[1])
+
+
+def print_move(c):
+    print("MOVE " + str(c[0]) + " " + str(c[1]))
+
+def dist(c1,c2):
+    return int(math.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2))
+
 
 Enti = namedtuple('Enti', ['id', 'coord', 'type','state','val'])
 #Status = namedtuple('Status_map', ['explor', 'num_phan', 'num_bust'])
 
+#classes which defines action to perform
+
+class ExploreMap:
+    def __init__(self,agent_,map_explor_):
+        self.mx = map_explor_
+        self.agent = agent_
+        self.done = False
+        
+    def perform_action(self):
+        mc = self.mx.default_coord(self.agent.bust.coord)
+        print_move(mc)
+        self.done = True
+        
+class TakePhantom:
+    #2 thing to do : be a the right distance and take the phantom
+    def __init__(self,agent_,phanid_):
+        self.agent = agent_
+        self.phan_id = phanid_
+        self.done = False
+        
+    def perform_action(self):
+        bust = self.agent.bust
+        phan = self.agent.view_ents['phan'][self.phan_id]
+        dist_b_p = dist(bust.coord,phan.coord)
+        if dist_b_p < 1760 and dist_b_p > 900:
+            print("BUST " + str(phan.id))
+            self.done = True
+        else:
+            #on sapproche
+            print_move(phan.coord)
+
+
+class StunOpo:
+    #2 thing to do : be a the right distance and stun
+    def __init__(self,agent_,opoid_):
+        self.agent = agent_
+        self.opo_id = opoid_
+        self.done = False
+        
+    def perform_action(self):
+        bust = self.agent.bust
+        opo = self.agent.view_ents['opo'][self.opo_id]
+        dist_b_p = dist(bust.coord,opo.coord)
+        if dist_b_p < 1760:
+            print("STUN " + str(opo.id))
+            self.done = True
+        else:
+            #on sapproche
+            print_move(opo.coord)
 
 class Plateform_system:
     def __init__(self,num_agent_,team_id_):
@@ -29,10 +88,10 @@ class Plateform_system:
         for e in en_l:
             if e.type == self.team_id:
                 self.agents[e.id].bust = e
-                self.agents[e.id].view_ents['phan'] = []
-                self.agents[e.id].view_ents['opo'] = []
+                self.agents[e.id].view_ents['phan'] = {}
+                self.agents[e.id].view_ents['opo'] = {}
 
-        print(self.agents)
+        print(self.agents,file=sys.stderr)
         #now vievable object
         for e in en_l:
             if e.type == self.team_id: continue
@@ -40,18 +99,20 @@ class Plateform_system:
                 ag = self.agents[id_ag]
                 if dist(e.coord,ag.bust.coord) <= 2200:#viewable
                     if e.type == -1:
-                        ag.view_ents['phan'].append(e)
+                        ag.view_ents['phan'][e.id] = e
                     else:
-                        ag.view_ents['opo'].append(e) 
+                        ag.view_ents['opo'][e.id] = e
             
 class Agent:
-    def __init__(self,pf_,play_id_):
+    def __init__(self,map_ex_,pf_,play_id_):
         self.pf = pf_
+        self.map_ex = map_ex_
         self.play_id = play_id_
         self.pf.connect_agent(self)
         self.queue = deque()
-        self.view_ents = {'phan': [], 'opo': []}
+        self.view_ents = {'phan': {}, 'opo': {}}
         self.bust = 0
+        self.action_planed = 0
         
     def broadcast_msg(self,msg):
         self.pf.broadcast(msg)
@@ -63,10 +124,40 @@ class Agent:
     def get_avail_msg(self):
         return self.queue.pop()
 
+    def prepare_action(self):
+        #update the action (if done, 0)
+        if self.action_planed != 0 and self.action_planed.done:
+            self.action_planned = 0
+            
+        #first take message
+        msg = 0
+        try:
+            msg = self.get_avail_msg()
+        except IndexError:
+            #no message, try to choose an action if no action are planned
+            if self.action_planed == 0:
+                #we need to find an action
+                if self.view_ents['phan']: #ok not empty, take phantom
+                    for phan_id in self.view_ents['phan']:
+                        self.action_planed = TakePhantom(self,phan_id)
+                        break
+                elif self.view_ents['opo']: #ok not empty, bust enemi
+                    for opo_id in self.view_ents['opo']:
+                        self.action_planed = StunOpo(self,opo_id)
+                        break
+                else: #on se balade
+                    self.action_planed = ExploreMap(self,self.map_ex)
+                    
+                return True
+            else:
+                return True
+
+        else:
+            return False
 
 
     def print_action(self):
-        print("action")
+        self.action_planed.perform_action()
 
         
 class Map_explore:
@@ -122,12 +213,7 @@ class Map_explore:
         #raise Exception('Heuuuuu')
         
         
-def move_v(c):
-    return "MOVE " + str(c[0]) + " " + str(c[1])
 
-
-def dist(c1,c2):
-    return int(math.sqrt((c1[0]-c2[0])**2 + (c1[1]-c2[1])**2))
 
 # Send your busters out into the fog to trap ghosts and bring them home!
 
@@ -141,7 +227,7 @@ map_ex = Map_explore(my_team_id)
 
 pls = Plateform_system(busters_per_player,my_team_id)
 #if team_id = 0 -> player id begins at 0, else begin at play 
-agents = [Agent(pls,busters_per_player*my_team_id + x) for x in range(busters_per_player)]
+agents = [Agent(map_ex,pls,busters_per_player*my_team_id + x) for x in range(busters_per_player)]
 
 
 # game loop
@@ -174,63 +260,19 @@ while True:
         if entity_type == my_team_id: #mon bus
             map_ex.coord2zone((x,y))['explor'] = 1 #on a explore
 
+ 
 
+    
     pls.update_entities(enti)
     map_ex.print_map()
 
 
+    while not all([ag.prepare_action() for ag in agents]): pass
+
 
     for ag in agents:
-        print(ag.bust, ag.view_ents)
+        print(ag.bust, ag.view_ents,file=sys.stderr)
+        ag.print_action()
+    #break
 
-    quit()
-    #*******************************************
-    
-    #find the closer buster
-    closer_ph = {}
-    for eb in enti:
-        #print("tt ",eb, file=sys.stderr)
-        if eb.type == -1: #my_team_id  ok b
-            min = 1000000
-            closest_id = -1
-            for ep in enti:
-               if ep.type == my_team_id: #-1:
-                   if dist(ep.coord,eb.coord) < min:
-                       closest_id = ep.id
-                       min = dist(ep.coord,eb.coord) 
-            if closest_id != -1: closer_ph[closest_id] = eb
-           
-    #for to in closer_ph:
-    #   print(to, closer_ph[to], file=sys.stderr)
-        
-    for en in enti:
-        if en.type != my_team_id: continue
-        if en.state == 1: #on a un phantome
-            if dist(en.coord,base) < 1600:
-                print("RELEASE")
-            else:
-                print("MOVE " + str(base[0]) + " " + str(base[1]))
-        elif en.id in closer_ph:
-            #on il voit un phantome
-            dist_b_p = dist(en.coord,closer_ph[en.id].coord)
-            if dist_b_p < 1760 and dist_b_p > 900:
-                #on prend le phanto mais avant on met a jour la map
-                map_ex.coord2zone(closer_ph[en.id].coord)['num_phan'] -= 1
-                
-                
-                print("BUST " + str(closer_ph[en.id].id))
-            else:
-                #on sapproche
-                print("MOVE " + str(closer_ph[en.id].coord[0]) + " " + str(closer_ph[en.id].coord[1]))
-                
-            print(dist_b_p,file=sys.stderr)
-
-            
-        else:    
-            # Write an action using print
-            # To debug: print("Debug messages...", file=sys.stderr)
-
-        # MOVE x y | BUST id | RELEASE
-            #print(move_v(random.choice(eval_points)))
-            mc = map_ex.default_coord(en.coord)
-            print(move_v(mc)) 
+   
