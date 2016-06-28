73914059a64ca73101f90fedb0f8801ee66db898
Ospici Matthieu <matthieu.ospici@atos.net>
2016-06-28 11:03:05 +0200

    - wip in agent system, first architecture seems to work

--- codebusters/cb.py
+++ codebusters/cb.py
@@ -1,6 +1,7 @@
 import sys
 import math
 from collections import namedtuple
+from collections import deque
 import random
 
 
@@ -9,6 +10,65 @@ Enti = namedtuple('Enti', ['id', 'coord', 'type','state','val'])
 #Status = namedtuple('Status_map', ['explor', 'num_phan', 'num_bust'])
 
 
+class Plateform_system:
+    def __init__(self,num_agent_,team_id_):
+        self.num_agent = num_agent_
+        self.team_id = team_id_
+        self.agents = {}
+
+
+    def connect_agent(self,ag_):
+        self.agents[ag_.play_id] = ag_
+
+    def broadcast_msg(self,msg):
+        for ag in self.agent:
+            ag.queue.append(msg)
+
+    def update_entities(self,en_l):
+        #first update the status of the bust
+        for e in en_l:
+            if e.type == self.team_id:
+                self.agents[e.id].bust = e
+                self.agents[e.id].view_ents['phan'] = []
+                self.agents[e.id].view_ents['opo'] = []
+
+        print(self.agents)
+        #now vievable object
+        for e in en_l:
+            if e.type == self.team_id: continue
+            for id_ag in self.agents:
+                ag = self.agents[id_ag]
+                if dist(e.coord,ag.bust.coord) <= 2200:#viewable
+                    if e.type == -1:
+                        ag.view_ents['phan'].append(e)
+                    else:
+                        ag.view_ents['opo'].append(e) 
+            
+class Agent:
+    def __init__(self,pf_,play_id_):
+        self.pf = pf_
+        self.play_id = play_id_
+        self.pf.connect_agent(self)
+        self.queue = deque()
+        self.view_ents = {'phan': [], 'opo': []}
+        self.bust = 0
+        
+    def broadcast_msg(self,msg):
+        self.pf.broadcast(msg)
+
+    def send_msg(self,msg,bot_id):
+        bot_id.append(msg)
+
+        
+    def get_avail_msg(self):
+        return self.queue.pop()
+
+
+
+    def print_action(self):
+        print("action")
+
+        
 class Map_explore:
     def __init__(self,team_id):
         self.map =   [{'explor':0,'num_phan':0,'num_bust':0}  for x in range(8*5)]
@@ -75,15 +135,23 @@ busters_per_player = int(input())  # the amount of busters you control
 ghost_count = int(input())  # the amount of ghosts on the map
 my_team_id = int(input())  # if this is 0, your base is on the top left of the map, if it is one, on the bottom right
 
+#class instance
+map_ex = Map_explore(my_team_id)
+
+
+pls = Plateform_system(busters_per_player,my_team_id)
+#if team_id = 0 -> player id begins at 0, else begin at play 
+agents = [Agent(pls,busters_per_player*my_team_id + x) for x in range(busters_per_player)]
+
+
 # game loop
 base = (0,0)
-eval_points = [(16000,0),(16000,2000),(16000,4000),(16000,6000),(16000,8000),(14000,9000),(11000,9000),(9000,9000),(6000,9000),(2000,9000)]
 
 if my_team_id == 1:
     base = (16000,9000)
-    eval_points = [(0,0),(0,2000),(0,4000),(0,6000),(0,8000),(14000,0),(11000,0),(9000,0),(6000,0),(2000,0)]
 
-map_ex = Map_explore(my_team_id)
+    
+
 
 
     
@@ -106,8 +174,18 @@ while True:
         if entity_type == my_team_id: #mon bus
             map_ex.coord2zone((x,y))['explor'] = 1 #on a explore
 
+
+    pls.update_entities(enti)
     map_ex.print_map()
-            
+
+
+
+    for ag in agents:
+        print(ag.bust, ag.view_ents)
+
+    quit()
+    #*******************************************
+    
     #find the closer buster
     closer_ph = {}
     for eb in enti:
